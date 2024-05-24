af0ca148be14b9c8da7431370ee88ee34bd43542
GigiKitty <alelysainz938@gmail.com>
2024-05-24 00:58:02 -0700

    crud ventas

--- CapaPresentacion/CRUD/CRUDventas.Designer.cs
+++ CapaPresentacion/CRUD/CRUDventas.Designer.cs
@@ -30,25 +30,257 @@
         {
             this.fondo = new System.Windows.Forms.Panel();
             this.barraTitulo = new System.Windows.Forms.Panel();
+            this.tblregistroventas = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.fondo.SuspendLayout();
+            this.barraTitulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tblregistroventas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // fondo
             // 
-            this.fondo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.fondo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(216)))), ((int)(((byte)(214)))));
+            this.fondo.Controls.Add(this.button3);
+            this.fondo.Controls.Add(this.button2);
+            this.fondo.Controls.Add(this.button1);
+            this.fondo.Controls.Add(this.numericUpDown1);
+            this.fondo.Controls.Add(this.textBox7);
+            this.fondo.Controls.Add(this.textBox6);
+            this.fondo.Controls.Add(this.textBox5);
+            this.fondo.Controls.Add(this.textBox4);
+            this.fondo.Controls.Add(this.textBox2);
+            this.fondo.Controls.Add(this.textBox1);
+            this.fondo.Controls.Add(this.label8);
+            this.fondo.Controls.Add(this.label7);
+            this.fondo.Controls.Add(this.label6);
+            this.fondo.Controls.Add(this.label5);
+            this.fondo.Controls.Add(this.label4);
+            this.fondo.Controls.Add(this.label3);
+            this.fondo.Controls.Add(this.label1);
+            this.fondo.Controls.Add(this.tblregistroventas);
             this.fondo.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.fondo.Location = new System.Drawing.Point(0, 40);
+            this.fondo.Location = new System.Drawing.Point(0, 74);
             this.fondo.Name = "fondo";
-            this.fondo.Size = new System.Drawing.Size(1092, 725);
+            this.fondo.Size = new System.Drawing.Size(1092, 691);
             this.fondo.TabIndex = 1;
             // 
             // barraTitulo
             // 
-            this.barraTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(86)))), ((int)(((byte)(200)))));
+            this.barraTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(234)))), ((int)(((byte)(239)))));
+            this.barraTitulo.Controls.Add(this.label2);
             this.barraTitulo.Dock = System.Windows.Forms.DockStyle.Top;
             this.barraTitulo.Location = new System.Drawing.Point(0, 0);
             this.barraTitulo.Name = "barraTitulo";
-            this.barraTitulo.Size = new System.Drawing.Size(1092, 40);
+            this.barraTitulo.Size = new System.Drawing.Size(1092, 74);
             this.barraTitulo.TabIndex = 0;
+            // 
+            // tblregistroventas
+            // 
+            this.tblregistroventas.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(222)))), ((int)(((byte)(220)))));
+            this.tblregistroventas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblregistroventas.Location = new System.Drawing.Point(535, 24);
+            this.tblregistroventas.Name = "tblregistroventas";
+            this.tblregistroventas.RowHeadersWidth = 51;
+            this.tblregistroventas.RowTemplate.Height = 24;
+            this.tblregistroventas.Size = new System.Drawing.Size(545, 355);
+            this.tblregistroventas.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(62)))), ((int)(((byte)(152)))));
+            this.label1.Location = new System.Drawing.Point(42, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IdEmpleado:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(62)))), ((int)(((byte)(152)))));
+            this.label2.Location = new System.Drawing.Point(399, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(287, 45);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Registro ventas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(62)))), ((int)(((byte)(152)))));
+            this.label3.Location = new System.Drawing.Point(42, 547);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Id:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(62)))), ((int)(((byte)(152)))));
+            this.label4.Location = new System.Drawing.Point(42, 456);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 27);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Importe:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(62)))), ((int)(((byte)(152)))));
+            this.label5.Location = new System.Drawing.Point(42, 373);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 27);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Descripcion:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(62)))), ((int)(((byte)(152)))));
+            this.label6.Location = new System.Drawing.Point(42, 296);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 27);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Cantidad:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(62)))), ((int)(((byte)(152)))));
+            this.label7.Location = new System.Drawing.Point(42, 221);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(168, 27);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Precio Unitario:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(62)))), ((int)(((byte)(152)))));
+            this.label8.Location = new System.Drawing.Point(42, 149);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(135, 27);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Id Producto:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(216)))), ((int)(((byte)(214)))));
+            this.textBox1.Location = new System.Drawing.Point(247, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 22);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(216)))), ((int)(((byte)(214)))));
+            this.textBox2.Location = new System.Drawing.Point(247, 373);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(240, 22);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(216)))), ((int)(((byte)(214)))));
+            this.textBox4.Location = new System.Drawing.Point(247, 226);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(240, 22);
+            this.textBox4.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(216)))), ((int)(((byte)(214)))));
+            this.textBox5.Location = new System.Drawing.Point(247, 149);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(240, 22);
+            this.textBox5.TabIndex = 15;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(216)))), ((int)(((byte)(214)))));
+            this.textBox6.Location = new System.Drawing.Point(247, 547);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(240, 22);
+            this.textBox6.TabIndex = 16;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(216)))), ((int)(((byte)(214)))));
+            this.textBox7.Location = new System.Drawing.Point(247, 461);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(240, 22);
+            this.textBox7.TabIndex = 17;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(253, 293);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(109, 22);
+            this.numericUpDown1.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(62)))), ((int)(((byte)(152)))));
+            this.button1.Location = new System.Drawing.Point(620, 585);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 42);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(62)))), ((int)(((byte)(152)))));
+            this.button2.Location = new System.Drawing.Point(944, 585);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 42);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(62)))), ((int)(((byte)(152)))));
+            this.button3.Location = new System.Drawing.Point(786, 585);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 42);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Editar";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // CRUDventas
             // 
@@ -60,6 +292,12 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "CRUDventas";
             this.Text = "CRUDventas";
+            this.fondo.ResumeLayout(false);
+            this.fondo.PerformLayout();
+            this.barraTitulo.ResumeLayout(false);
+            this.barraTitulo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tblregistroventas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -67,5 +305,24 @@
         #endregion
         private System.Windows.Forms.Panel fondo;
         private System.Windows.Forms.Panel barraTitulo;
+        private System.Windows.Forms.DataGridView tblregistroventas;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
